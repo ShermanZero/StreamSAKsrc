8448e9927b73091cc0e2b7c745a755c42de02bc9
ShermanZero <16752746+ShermanZero@users.noreply.github.com>
2018-04-12 20:50:06 -0400

    updates

---
+++ src/test/java/StreamSAK/StreamSAKTest.java
@@ -0,0 +1,10 @@
+package StreamSAK;
+
+import org.junit.Test;
+
+public class StreamSAKTest {
+	@Test public void testStreamSAK() {
+        StreamSAK s = new StreamSAK();
+        s.start();
+    }
+}
